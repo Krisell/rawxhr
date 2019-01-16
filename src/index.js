56a78e6f4c6fff8a1849d64c58ae4441530c275a
Martin Krisell <martin.krisell@gmail.com>
2019-01-16 12:25:20 +0100

    Add tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,114 @@
-export default function RawXHR () {
+import BetterArguments from '@krisell/better-arguments'
 
-} 
+/**
+ * Simple wrapper for RAW XMLHttpRequest Ajax calls.
+ * Martin Krisell 2018
+ *
+ * Normally, I use a more battle-tested Ajax-library like jQuery or Axios,
+ * but sometimes Ajax-calls need to be sent with no additonal headers (e.g. a CSRF-token).
+ * This library will send no headers (unless given explicitly) which also prevents
+ * unecessary preflights and rejections by APIs.
+ */
+export default function RawXHR (...specs) {
+  const options = BetterArguments.build({
+    specs,
+    defaultOptions: {
+      method: 'get',
+      contentType: 'json',
+      responseType: 'json'
+    },
+    namedOptions: ['url', 'method', 'responseType']
+  })
 
+  /**
+   * Initiate the XHR-object. Works in IE >= 10, which is enough for me.
+   */
+  const XMLHttpRequest = window.XMLHttpRequest
+  const xhr = new XMLHttpRequest()
+  xhr.open(options.method, options.url)
+
+  if (options.responseType) {
+    xhr.responseType = options.responseType
+  }
+
+  this.send = function (data) {
+    return new Promise((resolve, reject) => {
+      /**
+       * Attach listener before the send-event is initiated.
+       */
+      xhr.onerror = () => reject(new Error('XHR request failed'))
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 200 && xhr.status < 400) {
+          let data = xhr.response
+          if (options.responseType === 'json') {
+            if (typeof data === 'string') {
+              data = JSON.parse(data)
+            }
+          }
+
+          return resolve(data)
+        }
+
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status >= 400) {
+          reject(new Error('XHR request failed'))
+        }
+      }
+
+      if (options.method.toLowerCase() === 'post') {
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+        xhr.send(param(data))
+      } else {
+        xhr.send()
+      }
+    })
+  }
+}
+
+function param (data) {
+  let params = []
+
+  function add (key, value) {
+    if (typeof value === 'function') {
+      value = value()
+    }
+
+    if (value == null) { // Or undefined
+      value = ''
+    }
+
+    params.push(`${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+  }
+
+  function buildParams (prefix, obj) {
+    var i, len, key;
+
+    if (prefix) {
+      if (Array.isArray(obj)) {
+        for (i = 0, len = obj.length; i < len; i++) {
+            buildParams(
+                prefix + '[' + (typeof obj[i] === 'object' && obj[i] ? i : '') + ']',
+                obj[i]
+            );
+        }
+          } else if (String(obj) === '[object Object]') {
+              for (key in obj) {
+                  buildParams(prefix + '[' + key + ']', obj[key]);
+              }
+          } else {
+              add(prefix, obj);
+          }
+      } else if (Array.isArray(obj)) {
+          for (i = 0, len = obj.length; i < len; i++) {
+              add(obj[i].name, obj[i].value);
+          }
+      } else {
+          for (key in obj) {
+              buildParams(key, obj[key]);
+          }
+      }
+
+      return params
+  }
+
+  return buildParams('', data).join('&')
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,96 @@
 import RawXHR from '../lib/index.js'
 
-test('it is a function', () => {
-    expect(typeof RawXHR).toBe('function')
+describe('XHR calls', () => {
+    const oldXMLHttpRequest = window.XMLHttpRequest;
+    let mockXHR
+
+    afterEach(() => {
+        window.XMLHttpRequest = oldXMLHttpRequest
+    })
+
+    beforeEach(() => {
+        mockXHR = {
+            open: jest.fn(),
+            send: jest.fn(),
+            setRequestHeader: jest.fn(),
+            status: 200,
+            readyState: 4,
+            responseText: JSON.stringify(
+                [
+                    { title: 'test post' },
+                    { tile: 'second test post' }
+                ]
+            )
+        }
+        const oldXMLHttpRequest = window.XMLHttpRequest
+        window.XMLHttpRequest = jest.fn(() => mockXHR)
+
+        window.XMLHttpRequest.DONE = 4
+    })
+
+    test('it is a function', () => {
+        expect(typeof RawXHR).toBe('function')
+    })
+
+    test('can make a simple GET request', () => {
+        ;(new RawXHR('google.com')).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('get', 'google.com')
+
+        ;(new RawXHR({ url: 'google2.com' })).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('get', 'google2.com')
+    })
+
+    test('can specify HTTP verb', () => {
+        ;(new RawXHR('martin.se', 'get')).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('get', 'martin.se')
+
+        ;(new RawXHR('martin.se', 'post')).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('post', 'martin.se')
+
+        ;(new RawXHR('martin.se', { method: 'put' })).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('put', 'martin.se')
+
+        ;(new RawXHR({ url: 'martin.se', method: 'patch' })).send()
+        expect(mockXHR.open).toHaveBeenCalledWith('patch', 'martin.se')
+    })
+
+    test('JSON is converted to an object', () => {
+        let req = new RawXHR('/api')
+
+        mockXHR.response = JSON.stringify({ a: 2 })
+
+        let prom = req.send()
+        mockXHR.onreadystatechange()
+
+        expect.assertions(1)
+        return prom.then(data => {
+            expect(data).toEqual({ a: 2 })
+        })
+    })
+
+    test('Non-json is returned as a string', () => {
+        let req = new RawXHR('/api', { responseType: 'text' })
+
+        mockXHR.response = JSON.stringify({ a: 2 })
+
+        let prom = req.send()
+        mockXHR.onreadystatechange()
+
+        expect.assertions(1)
+        return prom.then(data => {
+            expect(data).toEqual(JSON.stringify({ a: 2 }))
+        })
+    })
+
+    test('Post data is sent correctly', () => {
+        let req = new RawXHR('/api', 'post')
+
+        mockXHR.response = JSON.stringify({ a: 2 })
+
+        let prom = req.send({ my: 'data', is: 'here', more: 1 })
+        mockXHR.onreadystatechange()
+
+        expect(mockXHR.setRequestHeader).toHaveBeenCalledWith("Content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+        expect(mockXHR.send).toHaveBeenCalledWith('my=data&is=here&more=1')
+    })
 })
